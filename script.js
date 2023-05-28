e13ef5964c4a6a0ccbab692454f37316563965a1
Jefferson Mañale <manalejefferson@gmai.com>
2023-05-28 15:32:23 +0800

    Update HTML & CSS

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
- 
+const {createRoot} = ReactDOM;
+const {useState} = React;
+const skills = document.querySelector("#skills");
+const skillsDiv = createRoot(skills);
+
+const skill_images = ["https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-plain.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg", "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"];
+const skill_name = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL", "Bash", "Git"];
+const skillElements = [];
+
+function Skills() {
+    return (
+      <div id="skills">
+        {skill_images.map((image, index) => (
+            <div key={index} className="skill">
+            <div className="skill-content">
+              <div className="skill-icon">
+                <img src={image} />
+              </div>
+              <p className="skill-name">{skill_name[index]}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+skillsDiv.render(<Skills/>);
 
 // Event Listener for Navigating Sections
 const navLinks = document.querySelectorAll(".nav-link");
